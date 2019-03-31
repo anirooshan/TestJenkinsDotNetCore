4a1294c5071ccfb21be962a284a4460e0c0a4ee6
anirooshan <anirooshan@tendersearch.com.au>
2019-03-31 19:00:28 +1100

    initial files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestJenkinsDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
